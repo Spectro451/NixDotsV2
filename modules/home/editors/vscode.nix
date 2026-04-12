007283a6266f6d382cb411d961f5120e4d2ff10e
Spectro451 <bastyfarias30@gmail.com>
2026-04-12 00:32:54 -0400

    se agregaron los formater y refactorizacion de nvim, se intento aplicar temas a vscodium con el modulo de noctalia pero este por algun motivo no los lee. solucion sera hacerlo manual

--- modules/home/editors/vscode.nix
+++ modules/home/editors/vscode.nix
@@ -13,6 +13,7 @@
       adpyke.codesnap
       ritwickdey.liveserver
       formulahendry.auto-rename-tag
+      unthrottled.doki-theme
       jnoortheen.nix-ide
     ];
     profiles.default.userSettings = {
@@ -22,7 +23,7 @@
       "editor.formatOnSave" = true;
       "files.autoSave" = "onFocusChange";
       "workbench.iconTheme" = "material-icon-theme";
-      "workbench.colorTheme" = "NoctaliaTheme";
+      "workbench.colorTheme" = "5412c41d-f76b-4488-85a7-1ae1a63bbfcc";
       "[nix]"."editor.defaultFormatter" = "jnoortheen.nix-ide";
       "[nix]"."editor.formatOnSave" = true;
       "[javascript]"."editor.defaultFormatter" = "esbenp.prettier-vscode";

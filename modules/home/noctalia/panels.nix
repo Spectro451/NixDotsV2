b5ca117dafa43f152518cc03d78dc84d9295f152
Spectro451 <bastyfarias30@gmail.com>
2026-07-11 21:35:53 -0400

    cambie el lockscreen para que sea mas parecido al de noctaliav4

--- modules/home/noctalia/panels.nix
+++ modules/home/noctalia/panels.nix
@@ -8,39 +8,111 @@
         {type = "dark_mode";}
         {type = "notification";}
         {type = "caffeine";}
-        {type = "clipboard";}
+        {type = "wallpaper";}
       ];
     };
 
     desktop_widgets.enabled = false;
 
     lockscreen_widgets = {
-      enabled = false;
+      enabled = true;
       schema_version = 2;
-      widget_order = ["lockscreen-login-box@DP-2"];
+      widget_order = [
+        "lockscreen-login-box@DP-2"
+        "lockscreen-widget-0000000000000001"
+        "lockscreen-widget-0000000000000002"
+        "lockscreen-widget-0000000000000003"
+        "lockscreen-widget-0000000000000004"
+      ];
       grid = {
         cell_size = 16;
         major_interval = 4;
         visible = true;
       };
-      widget."lockscreen-login-box@DP-2" = {
-        box_height = 70.0;
-        box_width = 400.0;
-        cx = 960.0;
-        cy = 961.0;
-        output = "DP-2";
-        rotation = 0.0;
-        type = "login_box";
-        settings = {
-          background_color = "surface_variant";
-          background_opacity = 0.88;
-          background_radius = 12.0;
-          input_opacity = 1.0;
-          input_radius = 6.0;
-          show_caps_lock = true;
-          show_keyboard_layout = true;
-          show_login_button = true;
-          show_password_hint = true;
+      widget = {
+        "lockscreen-login-box@DP-2" = {
+          box_height = 70.0;
+          box_width = 400.0;
+          cx = 960.0;
+          cy = 961.0;
+          output = "DP-2";
+          rotation = 0.0;
+          type = "login_box";
+          settings = {
+            background_color = "surface_variant";
+            background_opacity = 0.88;
+            background_radius = 12.0;
+            input_opacity = 1.0;
+            input_radius = 6.0;
+            show_caps_lock = true;
+            show_keyboard_layout = true;
+            show_login_button = true;
+            show_password_hint = true;
+          };
+        };
+        lockscreen-widget-0000000000000001 = {
+          box_height = 128.0;
+          box_width = 464.0;
+          cx = 960.0;
+          cy = 284.0;
+          output = "DP-2";
+          rotation = 0.0;
+          type = "clock";
+          settings = {
+            background = true;
+            background_opacity = 0.74;
+            background_padding = 0;
+            center_text = true;
+            clock_style = "digital";
+            format = "{:%H:%M}";
+          };
+        };
+        lockscreen-widget-0000000000000002 = {
+          box_height = 400.0;
+          box_width = 288.0;
+          cx = 960.0;
+          cy = 692.0;
+          output = "DP-2";
+          rotation = 0.0;
+          type = "media_player";
+          settings = {
+            background = true;
+            hide_when_no_media = true;
+            layout = "vertical";
+            shadow = true;
+          };
+        };
+        lockscreen-widget-0000000000000003 = {
+          box_height = 352.0;
+          box_width = 448.0;
+          cx = 960.0;
+          cy = 620.0;
+          output = "DP-2";
+          rotation = 0.0;
+          type = "fancy_audio_visualizer";
+          settings = {
+            background = false;
+            bloom_intensity = 0.45;
+            inner_diameter = 1.0;
+            ring_opacity = 0.7;
+            rotation_speed = 0.3;
+            sensitivity = 1.4;
+            visualization_mode = "bars_rings";
+            wave_thickness = 1.3;
+          };
+        };
+        lockscreen-widget-0000000000000004 = {
+          box_height = 48.0;
+          box_width = 160.0;
+          cx = 960.0;
+          cy = 372.0;
+          output = "DP-2";
+          rotation = 0.0;
+          type = "clock";
+          settings = {
+            clock_style = "digital";
+            format = "{:%d-%m-%Y}";
+          };
         };
       };
     };
